cad25eab364a9d0711c702624fa91943de8e4601
Ebubekir YAZICI <79103394+Ebubekiryzc@users.noreply.github.com>
2022-09-03 22:56:46 +0300

    Created.

---
+++ src/projects/kodlama.io.devs/Kodlama.io.Devs.Application/Features/ProgrammingLanguages/Commands/DeleteProgrammingLanguage/DeleteProgrammingLanguageCommand.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using Kodlama.io.Devs.Application.Features.ProgrammingLanguages.DTOs;
+using Kodlama.io.Devs.Application.Features.ProgrammingLanguages.Rules;
+using Kodlama.io.Devs.Application.Services.Repositories.EntityFramework;
+using Kodlama.io.Devs.Domain.Entities;
+using MediatR;
+
+namespace Kodlama.io.Devs.Application.Features.ProgrammingLanguages.Commands.DeleteProgrammingLanguage
+{
+    public class DeleteProgrammingLanguageCommand : IRequest<DeletedProgrammingLanguageDTO>
+    {
+        public int Id { get; set; }
+
+        public class DeleteProgrammingLanguageCommandHandler : IRequestHandler<DeleteProgrammingLanguageCommand, DeletedProgrammingLanguageDTO>
+        {
+            private readonly IProgrammingLanguageRepository _programmingLanguageRepository;
+            private readonly IMapper _mapper;
+            private readonly ProgrammingLanguageBusinessRules _programmingLanguageBusinessRules;
+
+            public DeleteProgrammingLanguageCommandHandler(IProgrammingLanguageRepository programmingLanguageRepository, IMapper mapper, ProgrammingLanguageBusinessRules programmingLanguageBusinessRules)
+            {
+                _programmingLanguageRepository = programmingLanguageRepository;
+                _mapper = mapper;
+                _programmingLanguageBusinessRules = programmingLanguageBusinessRules;
+            }
+
+            public async Task<DeletedProgrammingLanguageDTO> Handle(DeleteProgrammingLanguageCommand request, CancellationToken cancellationToken)
+            {
+                await _programmingLanguageBusinessRules.ProgrammingLanguageMustExistWhenRequested(request.Id);
+
+                ProgrammingLanguage programmingLanguageToDelete = _programmingLanguageRepository.Get(p => p.Id == request.Id);
+                ProgrammingLanguage deletedProgrammingLanguage = await _programmingLanguageRepository.DeleteAsync(programmingLanguageToDelete);
+                DeletedProgrammingLanguageDTO deletedProgrammingLanguageDTO = _mapper.Map<DeletedProgrammingLanguageDTO>(deletedProgrammingLanguage);
+                return deletedProgrammingLanguageDTO;
+            }
+        }
+    }
+}
---
+++ src/projects/kodlama.io.devs/Kodlama.io.Devs.Application/Features/ProgrammingLanguages/Commands/DeleteProgrammingLanguage/DeleteProgrammingLanguageCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Kodlama.io.Devs.Application.Features.ProgrammingLanguages.Commands.DeleteProgrammingLanguage
+{
+    public class DeleteProgrammingLanguageCommandValidator : AbstractValidator<DeleteProgrammingLanguageCommand>
+    {
+        public DeleteProgrammingLanguageCommandValidator()
+        {
+            RuleFor(p => p.Id).NotNull();
+        }
+    }
+}
