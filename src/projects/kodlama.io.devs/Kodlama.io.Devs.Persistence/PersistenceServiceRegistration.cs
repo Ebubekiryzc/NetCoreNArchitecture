191e24b2ae017423ab3ac23832aa17c013f2419e
Ebubekir YAZICI <79103394+Ebubekiryzc@users.noreply.github.com>
2022-09-03 17:59:15 +0300

    Created service registration tool for Persistence layer.

---
+++ src/projects/kodlama.io.devs/Kodlama.io.Devs.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,23 @@
+﻿using Kodlama.io.Devs.Application.Services.Repositories.EntityFramework;
+using Kodlama.io.Devs.Persistence.Contexts;
+using Kodlama.io.Devs.Persistence.Repositories.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Kodlama.io.Devs.Persistence
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services,
+                                                                IConfiguration configuration)
+        {
+            services.AddDbContext<BaseDbContext>(options =>
+                                                     options.UseSqlServer(
+                                                         configuration.GetConnectionString("KodlamaIoDevsConnectionString")));
+            services.AddScoped<IProgrammingLanguageRepository, EFProgrammingLanguageRepository>();
+
+            return services;
+        }
+    }
+}
