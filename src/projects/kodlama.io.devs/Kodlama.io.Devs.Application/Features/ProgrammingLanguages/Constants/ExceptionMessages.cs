ed2c4bd6ccfff1363f8950a7077d39679d032fdb
Ebubekir YAZICI <79103394+Ebubekiryzc@users.noreply.github.com>
2022-09-03 22:57:44 +0300

    Added new exception message.

--- src/projects/kodlama.io.devs/Kodlama.io.Devs.Application/Features/ProgrammingLanguages/Constants/ExceptionMessages.cs
+++ src/projects/kodlama.io.devs/Kodlama.io.Devs.Application/Features/ProgrammingLanguages/Constants/ExceptionMessages.cs
@@ -3,5 +3,6 @@
     public static class ExceptionMessages
     {
         public const string ProgrammingLanguageNameExist = "A programming language with the given name already exists.";
+        public const string ProgrammingLanguageIsNull = "No programming language found matching these criteria.";
     }
 }
