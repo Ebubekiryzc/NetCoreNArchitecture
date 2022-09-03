f19e3211787f691eab96c30c4007dea651fa226c
Ebubekir YAZICI <79103394+Ebubekiryzc@users.noreply.github.com>
2022-09-03 23:00:04 +0300

    Created.

---
+++ src/projects/kodlama.io.devs/Kodlama.io.Devs.WebAPI/Controllers/ProgrammingLanguagesController.cs
@@ -0,0 +1,35 @@
+﻿using Kodlama.io.Devs.Application.Features.ProgrammingLanguages.Commands.CreateProgrammingLanguage;
+using Kodlama.io.Devs.Application.Features.ProgrammingLanguages.Commands.DeleteProgrammingLanguage;
+using Kodlama.io.Devs.Application.Features.ProgrammingLanguages.Commands.UpdateProgrammingLanguage;
+using Kodlama.io.Devs.Application.Features.ProgrammingLanguages.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kodlama.io.Devs.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProgrammingLanguagesController : BaseController
+    {
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] CreateProgrammingLanguageCommand createProgrammingLanguageCommand)
+        {
+            CreatedProgrammingLanguageDTO result = await Mediator.Send(createProgrammingLanguageCommand);
+            return Created("", result);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] UpdateProgrammingLanguageCommand updateProgrammingLanguageCommand)
+        {
+            UpdatedProgrammingLanguageDTO result = await Mediator.Send(updateProgrammingLanguageCommand);
+            return Ok(result);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete([FromBody] DeleteProgrammingLanguageCommand deleteProgrammingLanguageCommand)
+        {
+            DeletedProgrammingLanguageDTO result = await Mediator.Send(deleteProgrammingLanguageCommand);
+            return Ok(result);
+        }
+    }
+}
