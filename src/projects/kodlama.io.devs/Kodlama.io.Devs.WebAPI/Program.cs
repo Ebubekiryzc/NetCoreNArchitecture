cb69c9b6be340f1263f60c82dca91b2293f69cfd
Ebubekir YAZICI <79103394+Ebubekiryzc@users.noreply.github.com>
2022-09-03 17:59:38 +0300

    Injected persistence services.

--- src/projects/kodlama.io.devs/Kodlama.io.Devs.WebAPI/Program.cs
+++ src/projects/kodlama.io.devs/Kodlama.io.Devs.WebAPI/Program.cs
@@ -1,8 +1,12 @@
+using Kodlama.io.Devs.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddPersistenceServices(builder.Configuration);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
