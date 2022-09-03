9a6179eb090425b4961e252497434ecdaddccb63
Ebubekir YAZICI <79103394+Ebubekiryzc@users.noreply.github.com>
2022-09-03 22:58:45 +0300

    Added new business rules.

--- src/projects/kodlama.io.devs/Kodlama.io.Devs.Application/Features/ProgrammingLanguages/Rules/ProgrammingLanguageBusinessRules.cs
+++ src/projects/kodlama.io.devs/Kodlama.io.Devs.Application/Features/ProgrammingLanguages/Rules/ProgrammingLanguageBusinessRules.cs
@@ -3,6 +3,7 @@ using Core.Persistence.Paging;
 using Kodlama.io.Devs.Application.Features.ProgrammingLanguages.Constants;
 using Kodlama.io.Devs.Application.Services.Repositories.EntityFramework;
 using Kodlama.io.Devs.Domain.Entities;
+using System.Runtime.Intrinsics.Arm;
 
 namespace Kodlama.io.Devs.Application.Features.ProgrammingLanguages.Rules
 {
@@ -15,10 +16,22 @@ namespace Kodlama.io.Devs.Application.Features.ProgrammingLanguages.Rules
             _programmingLanguageRepository = programmingLanguageRepository;
         }
 
-        public async Task ProgrammingLanguageNameMustBeUniqueWhenSaving(string name)
+        public async Task ProgrammingLanguageNameMustBeUniqueWhenInserting(string name)
         {
             IPaginate<ProgrammingLanguage> result = await _programmingLanguageRepository.GetListAsync(p => p.Name == name);
             if (result.Items.Any()) throw new BusinessException(ExceptionMessages.ProgrammingLanguageNameExist);
+        }
+
+        public async Task ProgrammingLanguageNameMustBeUniqueWhenUpdating(int id, string name)
+        {
+            IPaginate<ProgrammingLanguage> result = await _programmingLanguageRepository.GetListAsync(p => p.Id != id && p.Name == name);
+            if (result.Items.Any()) throw new BusinessException(ExceptionMessages.ProgrammingLanguageNameExist);
+        }
+
+        public async Task ProgrammingLanguageMustExistWhenRequested(int id)
+        {
+            ProgrammingLanguage result = await _programmingLanguageRepository.GetAsync(p => p.Id == id);
+            if (result == null) throw new BusinessException(ExceptionMessages.ProgrammingLanguageIsNull);
         }
     }
 }
