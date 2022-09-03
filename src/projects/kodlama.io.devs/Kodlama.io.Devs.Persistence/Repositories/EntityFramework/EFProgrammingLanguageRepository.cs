a45c72a8bb44743c3087dbe4398fde61ce05cb3e
Ebubekir YAZICI <79103394+Ebubekiryzc@users.noreply.github.com>
2022-09-03 17:57:49 +0300

    Created.

---
+++ src/projects/kodlama.io.devs/Kodlama.io.Devs.Persistence/Repositories/EntityFramework/EFProgrammingLanguageRepository.cs
@@ -0,0 +1,19 @@
+﻿using Core.Persistence.Repositories;
+using Kodlama.io.Devs.Application.Services.Repositories.EntityFramework;
+using Kodlama.io.Devs.Domain.Entities;
+using Kodlama.io.Devs.Persistence.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kodlama.io.Devs.Persistence.Repositories.EntityFramework
+{
+    public class EFProgrammingLanguageRepository : EfRepositoryBase<ProgrammingLanguage, BaseDbContext>, IProgrammingLanguageRepository
+    {
+        public EFProgrammingLanguageRepository(BaseDbContext context) : base(context)
+        {
+        }
+    }
+}
