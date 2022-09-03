dc8c3f02649b56f867d9429d1a7f0562923ec679
Ebubekir YAZICI <79103394+Ebubekiryzc@users.noreply.github.com>
2022-09-03 21:00:14 +0300

    Added new mappings for create command.

---
+++ src/projects/kodlama.io.devs/Kodlama.io.Devs.Application/Features/ProgrammingLanguages/Profiles/MappingProfiles.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Kodlama.io.Devs.Application.Features.ProgrammingLanguages.Commands.CreateProgrammingLanguage;
+using Kodlama.io.Devs.Application.Features.ProgrammingLanguages.DTOs;
+using Kodlama.io.Devs.Domain.Entities;
+
+namespace Kodlama.io.Devs.Application.Features.ProgrammingLanguages.Profiles
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<ProgrammingLanguage, CreatedProgrammingLanguageDTO>().ReverseMap();
+            CreateMap<ProgrammingLanguage, CreateProgrammingLanguageCommand>().ReverseMap();
+        }
+    }
+}
