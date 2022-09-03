0045d291b43bc5639847ff1a150d79f24b105694
Ebubekir YAZICI <79103394+Ebubekiryzc@users.noreply.github.com>
2022-09-03 21:01:24 +0300

    Added uniqueness rule on name field.

---
+++ src/projects/kodlama.io.devs/Kodlama.io.Devs.Application/Features/ProgrammingLanguages/Rules/ProgrammingLanguageBusinessRules.cs
@@ -0,0 +1,24 @@
+﻿using Core.CrossCuttingConcerns.Exceptions;
+using Core.Persistence.Paging;
+using Kodlama.io.Devs.Application.Features.ProgrammingLanguages.Constants;
+using Kodlama.io.Devs.Application.Services.Repositories.EntityFramework;
+using Kodlama.io.Devs.Domain.Entities;
+
+namespace Kodlama.io.Devs.Application.Features.ProgrammingLanguages.Rules
+{
+    public class ProgrammingLanguageBusinessRules
+    {
+        private readonly IProgrammingLanguageRepository _programmingLanguageRepository;
+
+        public ProgrammingLanguageBusinessRules(IProgrammingLanguageRepository programmingLanguageRepository)
+        {
+            _programmingLanguageRepository = programmingLanguageRepository;
+        }
+
+        public async Task ProgrammingLanguageNameMustBeUnique(string name)
+        {
+            IPaginate<ProgrammingLanguage> result = await _programmingLanguageRepository.GetListAsync(p => p.Name == name);
+            if (result.Items.Any()) throw new BusinessException(ExceptionMessages.ProgrammingLanguageNameExist);
+        }
+    }
+}
