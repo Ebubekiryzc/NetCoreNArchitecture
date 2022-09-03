d754d4f9c4434e31943679fb52ad13eaf75a43c1
Ebubekir YAZICI <79103394+Ebubekiryzc@users.noreply.github.com>
2022-09-03 22:59:38 +0300

    Added new service injections.

--- src/projects/kodlama.io.devs/Kodlama.io.Devs.WebAPI/Program.cs
+++ src/projects/kodlama.io.devs/Kodlama.io.Devs.WebAPI/Program.cs
@@ -1,12 +1,22 @@
+using Application;
+using Core.CrossCuttingConcerns.Exceptions;
+using Kodlama.io.Devs.Application;
 using Kodlama.io.Devs.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddApplicationServices();
+builder.Services.AddSecurityServices();
 builder.Services.AddPersistenceServices(builder.Configuration);
+//builder.Services.AddInfrastructureServices();
+builder.Services.AddHttpContextAccessor();
 
+
+// Add services to the container.
+
+builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -19,6 +29,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+if (app.Environment.IsProduction())
+    app.ConfigureCustomExceptionMiddleware();
+
 
 app.UseHttpsRedirection();
 
