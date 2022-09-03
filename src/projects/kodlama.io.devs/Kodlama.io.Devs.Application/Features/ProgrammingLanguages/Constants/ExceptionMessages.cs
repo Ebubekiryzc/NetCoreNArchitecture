9ad4f6302566b3c31013462af5d1578273837156
Ebubekir YAZICI <79103394+Ebubekiryzc@users.noreply.github.com>
2022-09-03 20:59:30 +0300

    Added new constant for duplicated records.

---
+++ src/projects/kodlama.io.devs/Kodlama.io.Devs.Application/Features/ProgrammingLanguages/Constants/ExceptionMessages.cs
@@ -0,0 +1,7 @@
+﻿namespace Kodlama.io.Devs.Application.Features.ProgrammingLanguages.Constants
+{
+    public static class ExceptionMessages
+    {
+        public const string ProgrammingLanguageNameExist = "A programming language with the given name already exists.";
+    }
+}
