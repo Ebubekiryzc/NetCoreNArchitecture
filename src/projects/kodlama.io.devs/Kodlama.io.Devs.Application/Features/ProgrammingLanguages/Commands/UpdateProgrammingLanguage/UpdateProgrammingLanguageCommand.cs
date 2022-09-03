5479982eee23941d88c024c012bdfef337688003
Ebubekir YAZICI <79103394+Ebubekiryzc@users.noreply.github.com>
2022-09-03 21:28:26 +0300

    Created.

---
+++ src/projects/kodlama.io.devs/Kodlama.io.Devs.Application/Features/ProgrammingLanguages/Commands/UpdateProgrammingLanguage/UpdateProgrammingLanguageCommand.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using Kodlama.io.Devs.Application.Features.ProgrammingLanguages.DTOs;
+using Kodlama.io.Devs.Application.Features.ProgrammingLanguages.Rules;
+using Kodlama.io.Devs.Application.Services.Repositories.EntityFramework;
+using Kodlama.io.Devs.Domain.Entities;
+using MediatR;
+
+namespace Kodlama.io.Devs.Application.Features.ProgrammingLanguages.Commands.UpdateProgrammingLanguage
+{
+    public class UpdateProgrammingLanguageCommand : IRequest<UpdatedProgrammingLanguageDTO>
+    {
+        public string Name { get; set; }
+
+        public class UpdateProgrammingLanguageCommandHandler : IRequestHandler<UpdateProgrammingLanguageCommand, UpdatedProgrammingLanguageDTO>
+        {
+            private readonly IProgrammingLanguageRepository _programmingLanguageRepository;
+            private readonly IMapper _mapper;
+            private readonly ProgrammingLanguageBusinessRules _programmingLanguageBusinessRules;
+
+            public UpdateProgrammingLanguageCommandHandler(IProgrammingLanguageRepository programmingLanguageRepository, IMapper mapper, ProgrammingLanguageBusinessRules programmingLanguageBusinessRules)
+            {
+                _programmingLanguageRepository = programmingLanguageRepository;
+                _mapper = mapper;
+                _programmingLanguageBusinessRules = programmingLanguageBusinessRules;
+            }
+
+            public async Task<UpdatedProgrammingLanguageDTO> Handle(UpdateProgrammingLanguageCommand request, CancellationToken cancellationToken)
+            {
+                await _programmingLanguageBusinessRules.ProgrammingLanguageNameMustBeUniqueWhenSaving(request.Name);
+
+                ProgrammingLanguage mappedProgrammingLanguage = _mapper.Map<ProgrammingLanguage>(request);
+                ProgrammingLanguage updatedProgrammingLanguage = await _programmingLanguageRepository.UpdateAsync(mappedProgrammingLanguage);
+                UpdatedProgrammingLanguageDTO updatedProgrammingLanguageDTO = _mapper.Map<UpdatedProgrammingLanguageDTO>(updatedProgrammingLanguage);
+                return updatedProgrammingLanguageDTO;
+            }
+        }
+    }
+}
