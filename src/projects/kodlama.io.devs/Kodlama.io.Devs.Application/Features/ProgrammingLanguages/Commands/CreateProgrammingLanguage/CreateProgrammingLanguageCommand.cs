332c230fdeec69ad85ebb145ca2dd82b623760f7
Ebubekir YAZICI <79103394+Ebubekiryzc@users.noreply.github.com>
2022-09-03 20:58:40 +0300

    Created.

---
+++ src/projects/kodlama.io.devs/Kodlama.io.Devs.Application/Features/ProgrammingLanguages/Commands/CreateProgrammingLanguage/CreateProgrammingLanguageCommand.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using Kodlama.io.Devs.Application.Features.ProgrammingLanguages.DTOs;
+using Kodlama.io.Devs.Application.Features.ProgrammingLanguages.Rules;
+using Kodlama.io.Devs.Application.Services.Repositories.EntityFramework;
+using Kodlama.io.Devs.Domain.Entities;
+using MediatR;
+
+namespace Kodlama.io.Devs.Application.Features.ProgrammingLanguages.Commands.CreateProgrammingLanguage
+{
+    public class CreateProgrammingLanguageCommand : IRequest<CreatedProgrammingLanguageDTO>
+    {
+        public string Name { get; set; }
+
+        public class CreateProgrammingLanguageCommandHandler : IRequestHandler<CreateProgrammingLanguageCommand, CreatedProgrammingLanguageDTO>
+        {
+            private readonly IProgrammingLanguageRepository _programmingLanguageRepository;
+            private readonly IMapper _mapper;
+            private readonly ProgrammingLanguageBusinessRules _programmingLanguageBusinessRules;
+
+            public CreateProgrammingLanguageCommandHandler(IProgrammingLanguageRepository programmingLanguageRepository, IMapper mapper, ProgrammingLanguageBusinessRules programmingLanguageBusinessRules)
+            {
+                _programmingLanguageRepository = programmingLanguageRepository;
+                _mapper = mapper;
+                _programmingLanguageBusinessRules = programmingLanguageBusinessRules;
+            }
+
+            public async Task<CreatedProgrammingLanguageDTO> Handle(CreateProgrammingLanguageCommand request, CancellationToken cancellationToken)
+            {
+                await _programmingLanguageBusinessRules.ProgrammingLanguageNameMustBeUnique(request.Name);
+
+                ProgrammingLanguage mappedProgrammingLanguage = _mapper.Map<ProgrammingLanguage>(request);
+                ProgrammingLanguage createdProgrammingLanguage = await _programmingLanguageRepository.AddAsync(mappedProgrammingLanguage);
+                CreatedProgrammingLanguageDTO createdProgrammingLanguageDTO = _mapper.Map<CreatedProgrammingLanguageDTO>(createdProgrammingLanguage);
+                return createdProgrammingLanguageDTO;
+            }
+        }
+    }
+}
---
+++ src/projects/kodlama.io.devs/Kodlama.io.Devs.Application/Features/ProgrammingLanguages/DTOs/CreatedProgrammingLanguageDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Kodlama.io.Devs.Application.Features.ProgrammingLanguages.DTOs
+{
+    public class CreatedProgrammingLanguageDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
