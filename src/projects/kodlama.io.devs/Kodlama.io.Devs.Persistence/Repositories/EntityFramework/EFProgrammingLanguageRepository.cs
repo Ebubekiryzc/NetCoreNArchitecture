7c1752068c12fa7e49e9d287800a06cb7481434e
Ebubekir YAZICI <79103394+Ebubekiryzc@users.noreply.github.com>
2022-09-03 20:57:49 +0300

    Removed unnecessary imports.

--- src/projects/kodlama.io.devs/Kodlama.io.Devs.Persistence/Repositories/EntityFramework/EFProgrammingLanguageRepository.cs
+++ src/projects/kodlama.io.devs/Kodlama.io.Devs.Persistence/Repositories/EntityFramework/EFProgrammingLanguageRepository.cs
@@ -2,11 +2,6 @@
 using Kodlama.io.Devs.Application.Services.Repositories.EntityFramework;
 using Kodlama.io.Devs.Domain.Entities;
 using Kodlama.io.Devs.Persistence.Contexts;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Kodlama.io.Devs.Persistence.Repositories.EntityFramework
 {
