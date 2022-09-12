721e30da405b8f249dc092abad1e16e300662f0a
Ebubekir YAZICI <79103394+Ebubekiryzc@users.noreply.github.com>
2022-09-12 12:47:59 +0300

    Created authentication controller.

---
+++ src/projects/kodlama.io.devs/Kodlama.io.Devs.WebAPI/Controllers/AuthenticationController.cs
@@ -0,0 +1,26 @@
+﻿using Kodlama.io.Devs.Application.Features.Authentication.Commands.RegisterUser;
+using Kodlama.io.Devs.Application.Features.Authentication.DTOs;
+using Kodlama.io.Devs.Application.Features.Authentication.Queries.LoginUser;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kodlama.io.Devs.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : BaseController
+    {
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterCommand registerCommand)
+        {
+            CreatedAccessTokenDTO createdAccessTokenDTO = await Mediator.Send(registerCommand);
+            return Created("", createdAccessTokenDTO);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginQuery loginQuery)
+        {
+            CreatedAccessTokenDTO createdAccessTokenDTO = await Mediator.Send(loginQuery);
+            return Created("", createdAccessTokenDTO);
+        }
+    }
+}
