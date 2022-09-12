75a7a06ea5a37b90fc2948e939bc2a6e9b2f0105
Ebubekir YAZICI <79103394+Ebubekiryzc@users.noreply.github.com>
2022-09-12 12:42:04 +0300

    Created interfaces for repositories.

---
+++ src/projects/kodlama.io.devs/Kodlama.io.Devs.Application/Services/Repositories/EntityFramework/IOperationClaimRepository.cs
@@ -0,0 +1,9 @@
+﻿using Core.Persistence.Repositories;
+using Core.Security.Entities;
+
+namespace Kodlama.io.Devs.Application.Services.Repositories.EntityFramework
+{
+    public interface IOperationClaimRepository : IRepository<OperationClaim>, IAsyncRepository<OperationClaim>
+    {
+    }
+}
---
+++ src/projects/kodlama.io.devs/Kodlama.io.Devs.Application/Services/Repositories/EntityFramework/IUserProfileRepository.cs
@@ -0,0 +1,9 @@
+﻿using Core.Persistence.Repositories;
+using Kodlama.io.Devs.Domain.Entities;
+
+namespace Kodlama.io.Devs.Application.Services.Repositories.EntityFramework
+{
+    public interface IUserProfileRepository : IRepository<UserProfile>, IAsyncRepository<UserProfile>
+    {
+    }
+}
